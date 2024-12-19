e904e0fa102fa7c181fb1979dd9ab790e6f41b7e
BakhtadzeSandro <sandrobakhtadze1909@gmail.com>
2024-12-19 23:51:34 +0400

    add displaying previous orders functionality and responsiveness for orders summary page

--- src/app/pages/orders-summary/orders-summary.component.ts
+++ src/app/pages/orders-summary/orders-summary.component.ts
@@ -4,20 +4,26 @@ import { GroupedOrders, Order } from '../../models/order.model';
 import { OrderCardComponent } from '../../components/order-card/order-card.component';
 import { CommonModule } from '@angular/common';
 import { isEqual } from 'lodash';
+import { DatePickerModule } from 'primeng/datepicker';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-orders-summary',
   templateUrl: './orders-summary.component.html',
   styleUrls: ['./orders-summary.component.scss'],
   standalone: true,
-  imports: [OrderCardComponent, CommonModule],
+  imports: [OrderCardComponent, CommonModule, DatePickerModule, FormsModule],
   providers: [OrderService],
 })
 export class OrdersSummaryComponent implements OnInit {
   private orderService = inject(OrderService);
+  private router = inject(Router);
   private _originalOrders: Order[] | undefined;
   deliveryPrice = 4;
   allOrdersLength: number | undefined;
+
+  selectedDate: Date | undefined;
 
   similarOrdersFromDifferentUsers: any[] | undefined = [];
 
@@ -37,6 +43,14 @@ export class OrdersSummaryComponent implements OnInit {
   }
 
   constructor() {}
+
+  backToOrderPage() {
+    this.router.navigate(['order']);
+  }
+
+  retrieveOrdersForSpecificDate() {
+    this.getOrders(this.selectedDate);
+  }
 
   groupOrders(orders: Order[]): GroupedOrders[] {
     const groupedOrders: GroupedOrders[] = [];
@@ -66,15 +80,20 @@ export class OrdersSummaryComponent implements OnInit {
     return groupedOrders;
   }
 
+  getOrders(date: Date | undefined) {
+    if (date) {
+      const formattedDate = `${
+        date.getMonth() + 1
+      }-${date.getDate()}-${date.getFullYear()}`;
+      this.orderService.retrieveOrders(formattedDate).then((orders) => {
+        this._originalOrders = orders;
+        this.orders = this.groupOrders(orders);
+        this.allOrdersLength = orders?.length;
+      });
+    }
+  }
+
   ngOnInit() {
-    const today = new Date();
-    const formattedDate = `${
-      today.getMonth() + 1
-    }-${today.getDate()}-${today.getFullYear()}`;
-    this.orderService.retrieveOrders(formattedDate).then((orders) => {
-      this._originalOrders = orders;
-      this.orders = this.groupOrders(orders);
-      this.allOrdersLength = orders.length;
-    });
+    this.getOrders(new Date());
   }
 }
