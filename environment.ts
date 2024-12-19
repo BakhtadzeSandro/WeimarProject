bb151c9977e89cdfba53175327d10797b672be3b
BakhtadzeSandro <sandrobakhtadze1909@gmail.com>
2024-12-19 23:20:07 +0400

    add firebase config

--- environment.ts
+++ environment.ts
@@ -1,28 +1,18 @@
-// export const firebaseConfig = {
-//   apiKey: 'AIzaSyAS6Hz5FsOtUDa8puvzNxS-nSIDXQMlZZw',
-//   authDomain: 'weimar-e5ba3.firebaseapp.com',
-//   projectId: 'weimar-e5ba3',
-//   storageBucket: 'weimar-e5ba3.firebasestorage.app',
-//   messagingSenderId: '812133961030',
-//   appId: '1:812133961030:web:a4e1daef18d289a705a947',
-//   measurementId: 'G-VKZM0FZNX1',
-// };
-// export const firebaseConfig = {
-//   apiKey: process.env['NEXT_PUBLIC_FIREBASE_API_KEY'] || '',
-//   authDomain: process.env['NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN'] || '',
-//   projectId: process.env['NEXT_PUBLIC_FIREBASE_PROJECT_ID'] || '',
-//   storageBucket: process.env['NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET'] || '',
-//   messagingSenderId:
-//     process.env['NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID'] || '',
-//   appId: process.env['NEXT_PUBLIC_FIREBASE_APP_ID'] || '',
-//   measurementId: process.env['NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID'] || '',
-// };
-
 export const firebaseConfig = {
-  apiKey: process.env['NG_APP_FIREBASE_API_KEY'],
-  authDomain: process.env['NG_APP_FIREBASE_AUTH_DOMAIN'],
-  projectId: process.env['NG_APP_FIREBASE_PROJECT_ID'],
-  storageBucket: process.env['NG_APP_FIREBASE_STORAGE_BUCKET'],
-  messagingSenderId: process.env['NG_APP_FIREBASE_MESSAGING_SENDER_ID'],
-  appId: process.env['NG_APP_FIREBASE_APP_ID'],
+  apiKey: 'AIzaSyAS6Hz5FsOtUDa8puvzNxS-nSIDXQMlZZw',
+  authDomain: 'weimar-e5ba3.firebaseapp.com',
+  projectId: 'weimar-e5ba3',
+  storageBucket: 'weimar-e5ba3.firebasestorage.app',
+  messagingSenderId: '812133961030',
+  appId: '1:812133961030:web:a4e1daef18d289a705a947',
+  measurementId: 'G-VKZM0FZNX1',
 };
+
+// export const firebaseConfig = {
+//   apiKey: process.env['NG_APP_FIREBASE_API_KEY'],
+//   authDomain: process.env['NG_APP_FIREBASE_AUTH_DOMAIN'],
+//   projectId: process.env['NG_APP_FIREBASE_PROJECT_ID'],
+//   storageBucket: process.env['NG_APP_FIREBASE_STORAGE_BUCKET'],
+//   messagingSenderId: process.env['NG_APP_FIREBASE_MESSAGING_SENDER_ID'],
+//   appId: process.env['NG_APP_FIREBASE_APP_ID'],
+// };
