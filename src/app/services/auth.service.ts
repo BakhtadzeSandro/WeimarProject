bcaae7b71931e14f67d5d403d7fd1d9a67b75cd5
BakhtadzeSandro <sandrobakhtadze1909@gmail.com>
2024-12-30 18:38:20 +0400

    coment unnecessary code

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -25,15 +25,15 @@ export class AuthService {
     signInWithPopup(this.firebaseAuth, provider, browserPopupRedirectResolver)
       .then((result) => {
         const credential = GoogleAuthProvider.credentialFromResult(result);
-        const token = credential?.accessToken;
-        const user = result.user;
+        // const token = credential?.accessToken;
+        // const user = result.user;
         this.router.navigate(['order']);
       })
       .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        const email = error.customData.email;
-        const credential = GoogleAuthProvider.credentialFromError(error);
+        // const errorCode = error.code;
+        // const errorMessage = error.message;
+        // const email = error.customData.email;
+        // const credential = GoogleAuthProvider.credentialFromError(error);
       });
   }
 
